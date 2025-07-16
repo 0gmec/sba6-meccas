85ef07191074b3cb184355caf5d6ba7eb09e04d7
mlsfiles75@gmail.com <mlsfiles75@gmail.com>
2025-07-16 17:21:53 -0400

    updated the product.ts

--- models/Product.ts
+++ models/Product.ts
@@ -1,13 +1,35 @@
-class Product {
-    name: string
+class Products {
+    id: number
+    title: string
+    description: string
+    category: string
     price: number
+    discountedPercentage: number
+    sku: string
 
-    constructor (name: string, price: number) {
-        this.name = name
-        this.price = price 
-    }
+   constructor(id: number, title: string, description: string, category: string,
+    price: number, discountedPercentage: number, sku: string ) {
 
-    displayDetails(): string {
-        return `${this.name} costs $${this.price}`
+        this.id = id
+        this.title = title
+        this.description = description
+        this.category = category
+        this.price = price
+        this.discountedPercentage = discountedPercentage
+        this.sku = sku
+   
+      
     }
-}
+      
+ }
+    
+   
+    // "id": 1,
+    //   "title": "Essence Mascara Lash Princess",
+    //   "description": "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects. Achieve dramatic lashes with this long-lasting and cruelty-free formula.",
+    //   "category": "beauty",
+    //   "price": 9.99,
+    //   "discountPercentage": 7.17,
+    //   "sku": "RCH45Q1A",
+
+ 
