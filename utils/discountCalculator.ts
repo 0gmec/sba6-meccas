49cef366d92a4f2de632fa26413eff7cbf25acba
mlsfiles75@gmail.com <mlsfiles75@gmail.com>
2025-07-17 00:07:48 -0400

    added wrong file

--- utils/discountCalculator.ts
+++ utils/discountCalculator.ts
@@ -1,3 +1,7 @@
-function calculateDiscount() {
-
+function calculateDiscount(price: number, discountPercentage: number) {
+ if (price <= 0 && discountPercentage <= 0 ){
+   return +(price * (discountPercentage /100)).toFixed(2)
+ 
+ }
+ 
 }
--- utils/taxCalculator.ts
+++ utils/taxCalculator.ts
@@ -1,9 +1,2 @@
 
 
-function calculateTax(price: number, discountPercentage: number) {
- if (price <= 0 && discountPercentage <= 0 ){
-   return +(price * (discountPercentage /100)).toFixed(2)
- 
- }
- 
-}
