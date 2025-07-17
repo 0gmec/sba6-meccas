66590f00cb093cb87c24aec30f84cec774feef23
mlsfiles75@gmail.com <mlsfiles75@gmail.com>
2025-07-17 00:06:30 -0400

    updated taxcalculator

---
+++ utils/taxCalculator.ts
@@ -0,0 +1,9 @@
+
+
+function calculateTax(price: number, discountPercentage: number) {
+ if (price <= 0 && discountPercentage <= 0 ){
+   return +(price * (discountPercentage /100)).toFixed(2)
+ 
+ }
+ 
+}
