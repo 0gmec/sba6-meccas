f134eb9b59e92d4761f3bfb88fa86a97d1cdabf9
mlsfiles75@gmail.com <mlsfiles75@gmail.com>
2025-07-18 13:54:31 -0400

    updated api

--- services/apiServices.ts
+++ services/apiServices.ts
@@ -3,11 +3,12 @@ import  {Products} from "../models/Product"
 async function fetchProductData() {
     try {
 const response = await fetch('https://dummyjson.com/products')
-let productData = await response. json()
+const productData = await response. json()
 console.log(productData)
 
 }catch (error) {
-    console.log("Error:", error)
+    throw new Error("error fetching product data")
+    
 }
 
 }
