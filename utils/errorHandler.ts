7d884a7518fa6abfa484bd296b36c31e6a1ba1f2
mlsfiles75@gmail.com <mlsfiles75@gmail.com>
2025-07-18 14:39:26 -0400

    created custom  error for error handling

---
+++ utils/errorHandler.ts
@@ -0,0 +1,10 @@
+const fetchProductData = async () => {
+   try {
+const productData = await
+fetchProductData()
+console.log("fetch product data")
+throw new Error(" error failed")
+    } catch (error) {
+        console.log("Error fetching product data")
+    }
+    }
